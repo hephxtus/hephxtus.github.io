7af29aaccbdc569734b12a58f9d3bff1e765743c
hephxtus <joe.hodges2000@gmail.com>
2023-03-01 19:31:05 +1300

    mssing close

--- src/App.tsx
+++ src/App.tsx
@@ -312,6 +312,7 @@ function Socials() {
 
     <a href="reports/Joes_CV_2022_extended.pdf" target="_blank" rel="noreferrer">
       <img src="icons/icons8-life-cycle.gif" alt="CV" className="socicon" />
+    </a>
 
   </div>;
 
